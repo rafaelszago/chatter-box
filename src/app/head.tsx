bc0c43fa69a9eeba3b6e8f1a5780ce80de598834
Rafael Zago <rafaelszago@gmail.com>
2023-03-19 19:44:09 -0300

    feat: add favicon by color schema

---
+++ src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        href="favicon-light.ico"
+        rel="icon"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        href="favicon-dark.ico"
+        rel="icon"
+        media="(prefers-color-scheme: dark)"
+      />
+    </>
+  )
+}
