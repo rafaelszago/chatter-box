558a05d8a7988b3e0d0b4bfe55547497459159f4
Rafael Zago <rafaelszago@gmail.com>
2023-03-20 16:40:25 -0300

    feat: add go back link

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,5 +1,7 @@
-import { AuthCard } from '@/components'
+import { AuthCard } from '@/components/auth-card'
+import { ChevronLeft } from 'lucide-react'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'ChatterBox | Sing In',
@@ -9,8 +11,16 @@ export const metadata: Metadata = {
 export default function SignInPage() {
   return (
     <div className="mx-auto max-w-lg text-center">
-      <h1 className="text-3xl font-bold mb-12">Welcome to ChatterBox</h1>
+      <h1 className="text-3xl font-bold mb-12">Welcome to ChatterBox 👋</h1>
       <AuthCard />
+      <div className="w-full inline-flex justify-center mt-6">
+        <Link href="/">
+          <span className="text-sm flex items-center hover:underline hover:underline-offset-2">
+            <ChevronLeft size={14} className="mr-1" />
+            Back to home
+          </span>
+        </Link>
+      </div>
     </div>
   )
 }
