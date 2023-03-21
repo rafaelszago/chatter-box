831d2eecd6e7ad58f6839abaeb0067e6686ea209
Rafael Zago <rafaelszago@gmail.com>
2023-03-21 14:36:14 -0300

    feat: create user menu

---
+++ src/components/user-menu.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { User } from '@prisma/client'
+import { ArrowLeft, Settings } from 'lucide-react'
+import { signOut } from 'next-auth/react'
+import { Avatar } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuArrow,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from './ui/dropdown-menu'
+
+type UserMenuProps = {
+  user: Pick<User, 'image' | 'name'>
+}
+
+export function UserMenu({ user }: UserMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar
+          src={user.image ?? ''}
+          alt={user.name ?? 'profile picture'}
+          fallback={user.name ?? ''}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{user.name ?? ''}</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Settings size={12} /> Settings
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut({ callbackUrl: '/signin' })}>
+          <ArrowLeft size={12} /> Sign Out
+        </DropdownMenuItem>
+        <DropdownMenuArrow fill="white" />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
